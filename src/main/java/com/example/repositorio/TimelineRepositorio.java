3093400dd784c418e9a345b69cec2c9f0333fcfa
eyvind1 <eyvind.osco@ucsp.edu.pe>
2016-06-18 12:08:32 -0500

    repo

--- src/main/java/com/example/repositorio/TimelineRepositorio.java
+++ src/main/java/com/example/repositorio/TimelineRepositorio.java
@@ -1,7 +1,17 @@
 package com.example.repositorio;
 
-/**
- * Created by eyvind on 18/06/16.
- */
-public class TimelineRepositorio {
+import com.example.dominio.Administrador;
+import com.example.dominio.Cancion;
+import com.example.dominio.Timeline;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.util.List;
+
+public interface TimelineRepositorio extends CrudRepository<Timeline, Integer> {
+    List<Timeline> findAll();
 }
