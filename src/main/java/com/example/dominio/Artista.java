8c3d7073bd039f22ba1279836a6b85d5b14dbccc
gretelcanasa <gretel.canasa@ucsp.edu.pe>
2016-06-18 12:10:26 -0500

    Update Artista.java

--- src/main/java/com/example/dominio/Artista.java
+++ src/main/java/com/example/dominio/Artista.java
@@ -1,6 +1,25 @@
 package com.example.dominio;
 
-public class Artista {
-	private String nombre;
-	private String apellido;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@Entity
+public class Artista{
+	
+	@Id
+	@SequenceGenerator(name = "Artista_ID_GENERATOR", sequenceName = "Artista_ID_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Artista_ID_GENERATOR")
+	public Integer id;
+	public String nombre;	
+	public String genero;
+	
+	public Integer id_album;
+	public Integer id_cancion;
 }
