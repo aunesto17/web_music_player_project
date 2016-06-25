baaa25798ef0a3c2dcb68479e90cea15815ce158
Alex Baylon <aunesto15@gmail.com>
2016-06-25 10:25:06 -0500

    actualizado

--- src/main/java/com/example/dominio/Usuario.java
+++ src/main/java/com/example/dominio/Usuario.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
@@ -17,9 +18,15 @@ public class Usuario extends Persona {
 	@SequenceGenerator(name = "Persona_ID_GENERATOR", sequenceName = "Persona_ID_SEQ")
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Persona_ID_GENERATOR")
 	public Integer id;
-	public Integer followers;
-	public Integer following;
+	//Creo que esta forma no es la optima para los seguidores
+	//public Integer followers;
+	//public Integer following;
 	public Boolean state;
 	
+	@ManyToMany
+	@JoinTable(name = "following_followers",
+		joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"),
+		inverseJoinColumns = @JoinColumn(name = "FOLLOWER_ID", referencedColumnName = "ID"))
+	private List<Usuario> seguidores;
 }
 	
